b285933078a205b880df4a1dd7ca58786e389be7
Glitch (waiting-caribou) <none>
2018-12-05 17:53:39 +0000

    ⏱🍟 Updated with Glitch: ouput % to log because ui stalls

--- server.js
+++ server.js
@@ -42,16 +42,24 @@ app.post("/day05part2", function (request, response) {
   const data2 = input2.trim();
   const rx = /aA|bB|cC|dD|eE|fF|gG|hH|iI|jJ|kK|lL|mM|nN|oO|pP|qQ|rR|sS|tT|uU|vV|wW|xX|yY|zZ|Aa|Bb|Cc|Dd|Ee|Ff|Gg|Hh|Ii|Jj|Kk|Ll|Mm|Nn|Oo|Pp|Qq|Rr|Ss|Tt|Uu|Vv|Ww|Xx|Yy|Zz/;
   
+  /*
   let rxs = [];
   for (let a = 97; a < (97+26); a++) {
     rxs.push(new RegExp(String.fromCharCode(a), "ig"));
   }
+  */
+  const rxs = [/a/ig,/b/ig,/c/ig,/d/ig,/e/ig,/f/ig,/g/ig,/h/ig,/i/ig,/j/ig,/k/ig,/l/ig,/m/ig,/n/ig,/o/ig,/p/ig,/q/ig,/r/ig,/s/ig,/t/ig,/u/ig,/v/ig,/w/ig,/x/ig,/y/ig,/z/ig];
 
+  let i = 0;
   const length = rxs.reduce((chars, rxl) => {
     let output = data2;
     output = output.replace(rxl, "");
     while (rx.test(output)) output = output.replace(rx, "");
     chars = Math.min(chars, output.length);
+    
+    i++;
+    console.log(Math.round(i / 26 * 100) + "%");
+    
     return chars;
   }, data2.length);
   
