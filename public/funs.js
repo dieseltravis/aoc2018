f737e2eee1e25196235263876129168fe28deedf
Glitch (waiting-caribou) <none>
2018-12-17 03:58:40 +0000

    👰🌗 Updated with Glitch: refactored days 3 - 5

--- public/funs.js
+++ public/funs.js
@@ -114,16 +114,268 @@ wvxyz
       }
     },
     "day3": {
-      "part1": data => {},
-      "part2": data => {}
+      "part1": data => {
+        const parse = /\#(\d+)\s\@\s(\d+),(\d+):\s(\d+)x(\d+)/;
+        //                1          2     3       4     5
+        const data1 = data.trim().split("\n").map(o => {
+          let match = o.match(parse);
+          return {
+            id: parseInt(match[1], 10),
+            x: parseInt(match[2], 10),
+            y: parseInt(match[3], 10),
+            w: parseInt(match[4], 10),
+            h: parseInt(match[5], 10)
+          };
+        });
+        
+        const plot = data1.reduce((p, o) => {
+          let y = o.y + 1,
+              x = o.x + 1;
+          
+          for (let h = y, hh = y + o.h; h < hh; h++) {
+            for (let w = x, ww = x + o.w; w < ww; w++) {
+              if (!p[h]) {
+                p[h] = [];
+              }
+              if (!p[h][w]) {
+                p[h][w] = 1
+              } else {
+                p[h][w]++;
+              }
+            }
+          }
+
+          return p;
+        }, []);
+        
+        const conflict = plot.reduce((c, a) => {
+          a.forEach((p) => {
+            if (p > 1) {
+              c++;
+            }
+          });
+
+          return c;
+        }, 0);
+        
+        return conflict;
+      },
+      "part2": data => {
+        const parse = /\#(\d+)\s\@\s(\d+),(\d+):\s(\d+)x(\d+)/;
+        //                1          2     3       4     5
+        
+        const data2 = data.trim().split("\n").map(o => {
+          let match = o.match(parse);
+          return {
+            id: parseInt(match[1], 10),
+            x: parseInt(match[2], 10),
+            y: parseInt(match[3], 10),
+            w: parseInt(match[4], 10),
+            h: parseInt(match[5], 10),
+            hasConflict: false
+          };
+        });
+        
+        const plot = data2.reduce((p, o) => {
+          let y = o.y + 1,
+              x = o.x + 1;
+          
+          for (let h = y, hh = y + o.h; h < hh; h++) {
+            for (let w = x, ww = x + o.w; w < ww; w++) {
+              if (!p[h]) {
+                p[h] = [];
+              }
+              if (!p[h][w]) {
+                p[h][w] = o.id
+              } else {
+                data2.filter(oo => {
+                  return (oo.id === p[h][w]);
+                })[0].hasConflict = true;
+                p[h][w] = o.id;
+                o.hasConflict = true;
+              }
+            }
+          }
+
+          return p;
+        }, []);
+        
+        const noConflict = data2.filter(o => { return !o.hasConflict; });
+        
+        return noConflict[0].id;
+      }
     },
     "day4": {
-      "part1": data => {},
-      "part2": data => {}
+      "part1": data => {
+        const pattern = /\[1518-(\d\d)-(\d\d)\s(\d\d):(\d\d)\]\s(Guard \#(\d+) begins shift|falls asleep|wakes up)/;
+        //                       1      2       3      4         5        6
+        
+        const data1 = data.trim().split("\n").sort();
+        let counter = {};
+        let lastId = 0;
+        let asleep = new Date();
+        let awake = new Date();
+        
+        data1.forEach(l => {
+          let parsed = l.match(pattern),
+              month = Number(parsed[1]),
+              day = Number(parsed[2]),
+              hour = Number(parsed[3]),
+              min = Number(parsed[4]),
+              entry = parsed[5],
+              cmd = entry.split(" ")[0];
+          
+          if (cmd === "Guard") {
+            // new guard
+            lastId = parsed[6];
+            counter[lastId] = counter[lastId] || { minutesAsleep: 0, sleepMinute: []};
+          } else if (cmd === "falls") {
+            // asleep
+            asleep = new Date(2018, month, day, hour, min);
+          } else if (cmd === "wakes") {
+            // awake
+            awake = new Date(2018, month, day, hour, min);
+            
+            let diffMs = awake - asleep;
+            let diffMins = Math.round(((diffMs % 86400000) % 3600000) / 60000);
+            counter[lastId].minutesAsleep += diffMins;
+            
+            for (let x = asleep.getMinutes(), y = awake.getMinutes(); x < y; x++) {
+              counter[lastId].sleepMinute[x] = counter[lastId].sleepMinute[x] || 0;
+              counter[lastId].sleepMinute[x]++;
+            }
+          }
+              
+        });
+        
+        //console.table(counter[2851].sleepMinute);
+        
+        let maxKey = 0,
+            maxMins = 0;
+        for (let key in counter) {
+          if (counter[key].minutesAsleep > maxMins) {
+            maxKey = key;
+            maxMins = counter[key].minutesAsleep;
+          }
+        }
+        
+        let maxSleep = counter[maxKey].sleepMinute,
+            maxMin = -1,
+            maxMinSleep = 0;
+        for (let i = 0, l = maxSleep.length; i < l; i++) {
+          if (maxSleep[i] !== null && maxSleep[i] > maxMinSleep) {
+            maxMin = i;
+            maxMinSleep = maxSleep[i];
+          }
+        }
+        
+        //console.info(maxKey, maxMin, (maxKey * maxMin));
+        
+        return (maxKey * maxMin);
+      },
+      "part2": data => {
+        const pattern = /\[1518-(\d\d)-(\d\d)\s(\d\d):(\d\d)\]\s(Guard \#(\d+) begins shift|falls asleep|wakes up)/;
+        //                       1      2       3      4         5        6
+
+        const data2 = data.trim().split("\n").sort();
+        
+        let counter = {};
+        let lastId = 0;
+        let asleep = new Date();
+        let awake = new Date();
+        
+        data2.forEach(l => {
+          let parsed = l.match(pattern),
+              month = Number(parsed[1]),
+              day = Number(parsed[2]),
+              hour = Number(parsed[3]),
+              min = Number(parsed[4]),
+              entry = parsed[5],
+              cmd = entry.split(" ")[0];
+          
+          if (cmd === "Guard") {
+            // new guard
+            lastId = parsed[6];
+            counter[lastId] = counter[lastId] || { minutesAsleep: 0, sleepMinute: []};
+          } else if (cmd === "falls") {
+            // asleep
+            asleep = new Date(2018, month, day, hour, min);
+          } else if (cmd === "wakes") {
+            // awake
+            awake = new Date(2018, month, day, hour, min);
+            
+            let diffMs = awake - asleep;
+            let diffMins = Math.round(((diffMs % 86400000) % 3600000) / 60000);
+            counter[lastId].minutesAsleep += diffMins;
+            
+            for (let x = asleep.getMinutes(), y = awake.getMinutes(); x < y; x++) {
+              counter[lastId].sleepMinute[x] = counter[lastId].sleepMinute[x] || 0;
+              counter[lastId].sleepMinute[x]++;
+            }
+          }
+              
+        });
+        
+        let maxKey = 0,
+            maxMin = -1,
+            maxMinSleep = 0;
+
+        for (let key in counter) {
+          let guard = counter[key],
+              maxSleep = guard.sleepMinute;
+          
+          guard.maxMin = -1;
+          guard.maxMinSleep = 0;
+          
+          for (let i = 0, l = maxSleep.length; i < l; i++) {
+            if (maxSleep[i] !== null && maxSleep[i] > guard.maxMinSleep) {
+              guard.maxMin = i;
+              guard.maxMinSleep = maxSleep[i];
+            }
+          }
+          
+          if (guard.maxMinSleep > maxMinSleep) {
+            maxKey = key;
+            maxMin = guard.maxMin;
+            maxMinSleep = guard.maxMinSleep;
+          }
+        }
+        
+        //console.info(maxKey, maxMin, (maxKey * maxMin));
+        
+        return (maxKey * maxMin);
+      }
     },
     "day5": {
-      "part1": data => {},
-      "part2": data => {}
+      "part1": data => {
+        const rx = /aA|bB|cC|dD|eE|fF|gG|hH|iI|jJ|kK|lL|mM|nN|oO|pP|qQ|rR|sS|tT|uU|vV|wW|xX|yY|zZ|Aa|Bb|Cc|Dd|Ee|Ff|Gg|Hh|Ii|Jj|Kk|Ll|Mm|Nn|Oo|Pp|Qq|Rr|Ss|Tt|Uu|Vv|Ww|Xx|Yy|Zz/;
+        
+        const data1 = data.trim();
+        let output = data1;
+        while (rx.test(output)) output = output.replace(rx, "");
+        
+        return output.length;
+      },
+      "part2": data => {
+        const data2 = data.trim();
+        const rx = /aA|bB|cC|dD|eE|fF|gG|hH|iI|jJ|kK|lL|mM|nN|oO|pP|qQ|rR|sS|tT|uU|vV|wW|xX|yY|zZ|Aa|Bb|Cc|Dd|Ee|Ff|Gg|Hh|Ii|Jj|Kk|Ll|Mm|Nn|Oo|Pp|Qq|Rr|Ss|Tt|Uu|Vv|Ww|Xx|Yy|Zz/g;
+        const rxs = [/a/ig,/b/ig,/c/ig,/d/ig,/e/ig,/f/ig,/g/ig,/h/ig,/i/ig,/j/ig,/k/ig,/l/ig,/m/ig,/n/ig,/o/ig,/p/ig,/q/ig,/r/ig,/s/ig,/t/ig,/u/ig,/v/ig,/w/ig,/x/ig,/y/ig,/z/ig];
+
+        let i = 0;
+        const length = rxs.reduce((chars, rxl) => {
+          let output = data2;
+          output = output.replace(rxl, "");
+          while (rx.test(output)) output = output.replace(rx, "");
+          chars = Math.min(chars, output.length);
+
+          i++;
+          console.log(Math.round(i / 26 * 100) + "%");
+
+          return chars;
+        }, data2.length);
+
+        return length;
+      }
     },
     "day6": {
       "part1": data => {},
